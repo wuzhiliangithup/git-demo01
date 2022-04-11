51bed8b03e61be599d45bcd158207595029392e4
skygit <150227661@qq.com>
2022-04-11 16:24:09 +0800

    commit master third

--- git-demo01/src/main/java/com/sea/A.java
+++ git-demo01/src/main/java/com/sea/A.java
@@ -24,6 +24,7 @@ public class A {
     public static void main(String[] args) {
         System.out.println("hello git1");//第一次提交代码
         System.out.println("hello git1");//第二次提交代码
+        System.out.println("hello git3");//第三次提交代码
 
 
 
