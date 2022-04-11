ab9845affb16751412e3fe3ef775dbea625a3b04
skygit <150227661@qq.com>
2022-04-11 16:16:02 +0800

    commit git first

---
+++ git-demo01/src/main/java/com/sea/A.java
@@ -0,0 +1,34 @@
+package com.sea;
+
+public class A {
+    private  int a;
+    private  String b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("hello git1");//第一次提交代码
+
+
+
+
+
+
+    }
+
+}
